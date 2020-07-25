bd391e1cff9f0f4a943c32352ac9f9614085ca23
Danilo Araujo de Souza <danilo.souza@edu.unipar.br>
2020-07-25 11:04:01 -0300

    inicial

---
+++ src/components/ButtonAlert.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-danger" v-on:click="clickAlert()">
+      Alerta
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonAlert",
+  methods: {
+    clickAlert() {
+      alert("oi Imundo!!!");
+    },
+  },
+};
+</script>
