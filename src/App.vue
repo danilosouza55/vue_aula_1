394259ab5b41fd744814c773ee0ed9e1812ac956
Danilo Araujo de Souza <danilo.souza@edu.unipar.br>
2020-08-01 10:33:30 -0300

    compomente de senha

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <h3>Clique no botão a baixo</h3>
       <ButtonAlert btnTitle="Nome do botão" message="Web dev" />
       <CustomList />
+      <ViewPass />
     </div>
   </div>
 </template>
@@ -13,11 +14,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import ButtonAlert from "./components/ButtonAlert";
 import CustomList from "./components/CustomList";
+import ViewPass from "./components/ViewPass";
 
 export default {
   name: "App",
   components: {
     ButtonAlert,
+    ViewPass,
     CustomList,
   },
 };
---
+++ src/components/ViewPass.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="form-group">
+    <label for="password">Senha</label>
+    <div class="input-group">
+      <input
+        type="password"
+        id="password"
+        class="form-control"
+        aria-describedby="buttons"
+        v-model="senha"
+        v-if="view===false"
+      />
+      <input
+        type="text"
+        id="password"
+        class="form-control"
+        aria-describedby="buttons"
+        v-model="senha"
+        v-if="view===true"
+      />
+
+      <div class="input-group-append" id="buttons">
+        <button type="button" class="btn btn-outline-info" @click="mostrarSenha">
+          <span class="fa fa-eye"></span>
+        </button>
+      </div>
+      <small v-if="senha===''">Senha vazia</small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewPass",
+  data: () => {
+    return {
+      view: false,
+      senha: "",
+    };
+  },
+  methods: {
+    mostrarSenha() {
+      if (this.view === false) {
+        this.view = true;
+      } else {
+        this.view = false;
+      }
+    },
+  },
+};
+</script>
